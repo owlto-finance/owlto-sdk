e5dff1aceac512813de7ee46ab8394bfb65ddf75
wankai <wankaizhang@gmail.com>
2024-04-23 17:31:23 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { Bridge, BridgeOptions } from './bridge';
-export { NetworkType, BridgeStatus, ApiStatus, ApiResult, ApiError } from './bridge/common'
+export {
+  NetworkType,
+  BridgeStatus,
+  ApiStatus,
+  ApiResult,
+  ApiError,
+} from './bridge/common';
