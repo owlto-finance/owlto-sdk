bdf172c2a39cf46bfb54fb5c913f6ee5686616e4
wankai <wankaizhang@gmail.com>
2024-05-07 19:05:03 +0800

    add interface

--- src/bridge/pair_info.ts
+++ src/bridge/pair_info.ts
@@ -1,39 +1,39 @@
-import { requestApi } from './common';
+import { BridgeValue, requestApi } from './common';
 import { GetAllPairInfosPath, GetPairInfoPath } from './api';
+
+export interface PairInfo {
+  tokenName: string;
+  fromChainName: string;
+  toChainName: string;
+  fromChainId: string;
+  toChainId: string;
+  fromTokenAddress: string;
+  toTokenAddress: string;
+  fromTokenDecimals: number;
+  toTokenDecimals: number;
+  minValue: BridgeValue;
+  maxValue: BridgeValue;
+  contractAddress: string;
+}
 
 export interface GetPairInfoRequest {
   tokenName: string;
   fromChainName: string;
   toChainName: string;
-}
-
-export interface GetPairInfoResponse {
-  uiMinValue: number;
-  uiMaxValue: number;
+  valueIncludeGasFee: boolean;
 }
 
 export interface GetAllPairInfosRequest {
   category: string;
+  valueIncludeGasFee: boolean;
 }
 
 export interface GetAllPairInfosResponse {
-  pairInfos: {
-    tokenName: string;
-    fromChainName: string;
-    toChainName: string;
-    fromChainId: string;
-    toChainId: string;
-    fromTokenAddress: string;
-    toTokenAddress: string;
-    fromTokenDecimals: number;
-    toTokenDecimals: number;
-    uiMinValue: number;
-    uiMaxValue: number;
-  }[];
+  pairInfos: PairInfo[];
 }
 
 export class PairInfoManager {
-  async getPairInfo(req: GetPairInfoRequest): Promise<GetPairInfoResponse> {
+  async getPairInfo(req: GetPairInfoRequest): Promise<PairInfo> {
     const info = await requestApi(GetPairInfoPath, req);
     return info;
   }
